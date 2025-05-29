88e79713a38d589a47a35e404a389a9c39c79385
luongle0706 <ducluongdn3@gmail.com>
2025-05-29 16:52:00 +0700

    feat: CRUD apis for medicine requests

---
+++ SchoolMedical_BusinessLogic/Interface/IMedicineRequestService.cs
@@ -0,0 +1,20 @@
+﻿using SchoolMedical_DataAccess.DTOModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedical_BusinessLogic.Interface
+{
+    public interface IMedicineRequestService
+    {
+        Task<PagingModel<MedicineRequestResponseDto>> GetMedicineRequestsAsync(MedicineRequestFilterRequestDto request);
+        Task<MedicineRequestResponseDto?> GetMedicineRequestByIdAsync(string id);
+        Task<MedicineRequestResponseDto> CreateMedicineRequestAsync(CreateMedicineRequestRequestDto request);
+        Task<MedicineRequestResponseDto> UpdateMedicineRequestAsync(UpdateMedicineRequestRequestDto request);
+        Task<bool> DeleteMedicineRequestAsync(string id);
+        Task<List<MedicineRequestResponseDto>> GetMedicineRequestsByStudentAsync(string studentId);
+        Task<List<MedicineRequestResponseDto>> GetMedicineRequestsByRequesterAsync(string requesterId);
+    }
+}
