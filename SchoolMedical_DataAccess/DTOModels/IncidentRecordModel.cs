661f32b2f015ddd9e44e019d0cf33fc4f82aecad
ultrakhaicraft <ultrakhaicraft2004@gmail.com>
2025-06-11 09:40:11 +0700

    aa

---
+++ SchoolMedical_DataAccess/DTOModels/IncidentRecordModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMedical_DataAccess.DTOModels;
+
+
+public class IncidentRecordDetailModel
+{
+	public string Id { get; set; } = null!;
+
+	public string StudentId { get; set; } = null!;
+
+	public string HandleBy { get; set; } = null!;
+
+	public string? IncidentType { get; set; }
+
+	public string? Description { get; set; }
+
+	public DateTime DateOccurred { get; set; }
+
+	public string? Status { get; set; }
+}
+
+public class IncidentRecordCreateRequest
+{
+	public string StudentId { get; set; } = null!;
+	public string HandleBy { get; set; } = null!;
+	public string? IncidentType { get; set; }
+	public string? Description { get; set; }
+	public DateTime DateOccurred { get; set; }
+	public string? Status { get; set; }
+}
+
+public class IncidentRecordUpdateRequest
+{
+	public string StudentId { get; set; } = null!;
+	public string HandleBy { get; set; } = null!;
+	public string? IncidentType { get; set; }
+	public string? Description { get; set; }
+	public DateTime DateOccurred { get; set; }
+	public string? Status { get; set; }
+}
+
+public class IncidentRecordViewModel
+{
+	public string Id { get; set; } = null!;
+	public string StudentId { get; set; } = null!;
+	public string StudentName { get; set; } = null!; //Search StudentName bang cach lay object Student trong IncidentRecord
+	public string? IncidentType { get; set; } //Search Incident Type
+	public DateTime DateOccurred { get; set; } //Sort theo ascending date
+	public string? Status { get; set; }
+}
+
+
+
+
